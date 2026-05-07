d861de497a53e8aa08bcb1b9ef6adad9c4e65b6c
CookieMonsta27 <magir27@yahoo.de>
2026-05-07 19:30:58 +0200

    feat: New menu point "Werdegang" added  --> First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { Home } from './sites/home/home';
 import { AboutMeComponent } from './sites/about-me/about-me';
+import { WerdegangComponent } from './sites/werdegang/werdegang';
 import { PortfolioComponent } from './sites/portfolio/portfolio';
 
 export const routes: Routes = [
-  { path: 'home', component: Home },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: Home },
   { path: 'aboutMe', component: AboutMeComponent },
+  { path: 'werdegang', component: WerdegangComponent },
   { path: 'portfolio', component: PortfolioComponent },
 ];
