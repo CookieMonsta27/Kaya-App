1f15a8dfba95614051a2bc8ab49c6df95e5be625
CookieMonsta27 <magir27@yahoo.de>
2026-06-14 18:14:00 +0200

    feat: Adding footer

---
+++ src/app/footer-component/footer-component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-component',
+  templateUrl: './footer-component.html',
+  styleUrl: './footer-component.css',
+})
+export class FooterComponent {
+  company = {
+    name: 'Kaya Makes',
+    legalForm: 'Yvonne Beck',
+    addressLine1: 'Musterstraße 1',
+    postal: '12345 Musterstadt',
+    country: 'Deutschland',
+    phone: '+49 123 456789',
+    email: 'info@example.com',
+    managingDirector: 'Max Mustermann',
+    hrb: 'HRB 12345',
+    registryCourt: 'Amtsgericht Musterstadt',
+    vatId: 'DE123456789',
+  };
+
+  nowYear = new Date().getFullYear();
+}
